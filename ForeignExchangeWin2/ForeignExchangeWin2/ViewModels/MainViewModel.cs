079c722b51461a52aeb21aaad91cf27563deebf3
Zulu <jzuluaga55@gmail.com>
2017-08-11 21:20:56 -0500

    Convert CB to MVVM

---
+++ ForeignExchangeWin2/ForeignExchangeWin2/ViewModels/MainViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ForeignExchangeWin2.ViewModels
+{
+    public class MainViewModel
+    {
+    }
+}
